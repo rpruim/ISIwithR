9209ba87b5d849dd16768f818d77084715571a26
Randall Pruim <rpruim@gmail.com>
2014-06-24 07:04:54 -0400

    save new version of OrganDonor into Package/data

--- Package/01DataPolishing.R
+++ Package/01DataPolishing.R
@@ -4,3 +4,4 @@ load("OrganDonor.rda")
 names(OrganDonor) <- tolower(names(OrganDonor))
 OrganDonor <- mutate(OrganDonor, 
                      default = factor(default, levels=unique(default)))
+save(OrganDonor, file="../Package/data/OrganDonor.rda")
