b2695be551c38883cf06bdbc716819226673cdff
Randall Pruim <rpruim@gmail.com>
2014-06-24 07:47:49 -0400

    stub of documentaiton for OrganDonor data set.

---
+++ Package/R/datasets.R
@@ -0,0 +1,16 @@
+
+#' Organ Donation
+#' 
+#' How becoming an organ donor depends on how a person is asked.
+#' 
+#' @name OrganDonor
+#' @docType data
+#' @format A dataset with 161 observations on the following variables.
+#' \itemize{ 
+#' \item{\code{default}} 
+#'   {\code{opt-in}, \code{opt-out}, or \code{neutral}}
+#' \item{\code{donor}}
+#'   {\code{donor} or \code{not}}
+#'   }
+#' @keywords datasets
+NULL
