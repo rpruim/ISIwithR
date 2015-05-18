0794f6d41e68c3aa4cd794afbe45067b0713951c
Randall Pruim <rpruim@gmail.com>
2015-05-19 02:14:54 +1200

    moved to TabulatedData

---
+++ data-raw/TabulatedData/tabulated.R
@@ -0,0 +1,19 @@
+
+#' Tabulated Data Sets
+#'
+#' These data sets are pre-tabulated into 2-way tables
+#'
+#' @rdname tabulated
+#' @alias CellphoneRace	
+#' @alias MarriageViews	
+#' @alias Rebate
+#' @alias YawnTime
+#' @alias FriskAge		
+#' @alias NightLight1		
+#' @alias SmartphoneRace
+#' @alias HealthcareAge	
+#' @alias NightLight2		
+#' @alias Towels
+#' @keywords datasets
+
+NULL
